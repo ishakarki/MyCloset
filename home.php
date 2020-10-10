2e5521d8a3428bd0f3704148fe8dd5d5e463c289
vale5181 <vale5181@colorado.edu>
2020-10-10 13:13:05 -0600

    rough login page

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php include('includes/header.inc.php'); ?>
+<body class="text-center"> 
+    <h2>MY</h2>
+    <h1>CLOSET</h1>
+<div>
+<form class="form-signin">
+<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+</form>
+</div>
+</body>
