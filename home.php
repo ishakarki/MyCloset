48d37a5c67df06dd261e331c2e6564f782a05254
ishakarki <iska6979@colorado.edu>
2020-10-19 15:51:24 -0600

    images and tentative logo

--- home.php
+++ home.php
@@ -16,13 +16,15 @@
     <meta http-equiv="imagetoolbar" content="no" />
   
 </head>
-<body class="text-center"> 
-    <h2>MY</h2>
-    <h1>CLOSET</h1>
+<body class="text-center">
 
-<form class="form-signin" action = "includes/signin.php" method="post">
-<input type="username" name="inputUsername" class="form-control" placeholder="Username" required="" autofocus="">
-<input type="password" name="inputPassword" class="form-control" placeholder="Password" required="">
-<button class="btn btn-lg btn-primary btn-block" type="submit" name="submit-signin">Sign in</button>
-</form>
+    <img src="includes/images/closetlogo.png" alt="closet logo" style="width:400px;height:400px;">
+
+    <h4> Welcome to My Closet! Please sign in. <h4>
+
+    <form class="form-signin" action = "includes/signin.php" method="post">
+    <input type="username" name="inputUsername" class="form-control" placeholder="Username" required="" autofocus="">
+    <input type="password" name="inputPassword" class="form-control" placeholder="Password" required="">
+    <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit-signin">Sign in</button>
+    </form>
 </body>
