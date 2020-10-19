4c3225fd6eb193368f3e3cbc900c9e518fe499c0
ishakarki <iska6979@colorado.edu>
2020-10-19 17:07:58 -0600

    adding childen classes to clothing and some closet

---
+++ fullcloset.php
@@ -0,0 +1,8 @@
+<?php include 'clothing.php';
+
+class Closet {
+    private $bottoms = [];
+    private $shirts = [];
+    private $sweaters = [];
+    private $onepieces = [];
+}
--- includes/clothing.php
+++ includes/clothing.php
@@ -1,20 +1,20 @@
 <?php
 
 class Clothing{
-    private $type;
-    private  $color;
-    private $temp;
-    private $pattern;
-    private $occasion;
-    private $file_title;
+    protected $type;
+    protected $color;
+    protected $temp;
+    protected $pattern;
+    protected $occasion;
+    protected $file_title;
     function __contruct($type, $color, $temp, $pattern, $occasion, $file_title)
     {
-        $this->type = $type;
-        $this->color = $color;
-        $this->temp = $temp;
-        $this->pattern = $pattern;
-        $this->occasion = $occasion;
-        $this->file_title = $file_tile;
+        $this->type = $type; // string 
+        $this->color = $color; // string
+        $this->temp = $temp; // int
+        $this->pattern = $pattern; // bool
+        $this->occasion = $occasion; // int level
+        $this->file_title = $file_tile; // string
     }
     function get_type(){return $this->type;}
     function get_color(){return $this->color;}
@@ -22,4 +22,22 @@ class Clothing{
     function get_pattern(){return $this->pattern;}
     function get_occasion(){return $this->occasion;}
     function get_file_title(){return $this->file_title;}
-};
+}
+
+class Bottom extends Clothing {
+    // type can be: pants, shorts, skirt
+}
+
+// $blackskirt = new Bottom('skirt')
+
+class Shirt extends Clothing {
+    // tshirt, long shirt, tanktop, blouse
+}
+
+class Sweater extends Clothing {
+    // jacket, pullover
+}
+
+class Onepiece extends Clothing {
+    // dress, jumpsuit
+}
