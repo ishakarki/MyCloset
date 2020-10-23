51197576f4c87b8e31df57a30af0a3f3e8683634
vale5181 <vale5181@colorado.edu>
2020-10-22 22:31:53 -0600

    pink button

--- main.php
+++ main.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
+#Log in if the the login was successful
 if(!isset($_SESSION['userId']))
 {
     header("Location:signin.php");
     exit();
 }
+#the session is only valid for 30 minutes, it will automatically log out
 else
 {
     $now = time();
