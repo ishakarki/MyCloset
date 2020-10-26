4743841ab6d09a61a5d44bfe33fbcf8d07c8f400
ishakarki <iska6979@colorado.edu>
2020-10-25 22:32:25 -0600

    adding to closet.php and creating generator.php

--- includes/closet.php
+++ includes/closet.php
@@ -5,4 +5,18 @@ class Closet {
     private $shirts = [];
     private $sweaters = [];
     private $onepieces = [];
+
+    // template for returning arrays based on a condition
+    function get_bottoms($occasion)
+    {
+        $matches = []; // array of matched clothes
+        foreach($bottoms as $key=>$val) // https://www.dyn-web.com/php/arrays/iterate/
+        {
+            if(bottoms[$key].get_occasion()==$occasion) // does the .get_occasion() work here?
+            {
+                array_push($matches,bottoms[$key]);
+            }
+        }
+        return $matches;
+    }
 }
---
+++ includes/generator.php
@@ -0,0 +1,12 @@
+<?php include 'clothing.php';
+include 'closet.php';
+
+class Generator{
+    // do we store outfits here or in Closet?
+
+    // ignore function definitions just saving the function declarations
+    function find_options(){echo 'hi';}
+    function pick_out(){echo 'out';}
+    function color_match(){echo 'colormatch';}
+    function save_outfit(){echo 'outfit';}
+}
