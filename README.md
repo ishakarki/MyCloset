fed5cb0a6799600718feb93d7f79d60d7a58d9c6
vale5181 <vale5181@colorado.edu>
2020-11-16 22:53:21 -0700

    new clothing

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ VALUES
     ('firstname','lastname','yourusername',’yourpassword’
 );
 
->CREATE TABLE IF NOT EXISTS clothing( idclothing INT NOT NULL AUTO_INCREMENT, typeofclothing TINYTEXT, color TINYTEXT, temp INT, pattern TINYTEXT, occasion INT, file_title LONGTEXT, PRIMARY KEY(idclothing) 
+>CREATE TABLE IF NOT EXISTS clothing( idclothing INT NOT NULL AUTO_INCREMENT, typeofclothing TINYTEXT, color TINYTEXT, pattern TINYTEXT, occasion INT, file_title LONGTEXT, PRIMARY KEY(idclothing) 
 );
 
 >CREATE TABLE IF NOT EXISTS outfits( idOutfit INT NOT NULL AUTO_INCREMENT, name TINYTEXT, bottoms INT, shirts INT, sweaters INT, onepieces INT, temperature INT, occasion INT, PRIMARY KEY(idOutfit) );
--- includes/readdata.php
+++ includes/readdata.php
@@ -1,7 +1,46 @@
 <?php
 require 'database_setup.php';
-
-$sql = "SELECT * FROM clothing;";
+include 'clothing.php';
+//sql statement to select the following columns in order to create clothing object
+$sql = "SELECT nameofclothing, typeofclothing, color, pattern, occasion, file_title FROM clothing;";
 $stmt = mysqli_stmt_init($conn);
+
+//check for connection error
+if(!mysqli_stmt_prepare($stmt, $sql))
+{
+    echo "fail connection";
+    exit();
+}
+else{
+    //execute the sql statement
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    //get every row in the database
+    while($row = mysqli_fetch_assoc($result))
+    {
+        echo $row["nameofclothing"] .$row["typeofclothing"] .$row["color"] .$row["pattern"] .$row["occasion"] .$row["file_title"];
+        //adding bottom 
+        if ($row["typeofclothing"] == "Bottom")
+        {
+            $bottom_piece = new Bottom($row["nameofclothing"], $row["typeofclothing"], $row["color"], $row["pattern"], $row["occasion"], $row["file_title"]);
+        }
+        //adding top
+        elseif ($row["typeofclothing"] == "Top")
+        {
+            $top_piece = new Top($row["nameofclothing"], $row["typeofclothing"], $row["color"], $row["pattern"], $row["occasion"], $row["file_title"]);
+        }
+        //adding sweater
+        elseif($row["typeofclothing"] == "Sweater")
+        {
+            $sweater_piece = new Sweater($row["nameofclothing"], $row["typeofclothing"], $row["color"], $row["pattern"], $row["occasion"], $row["file_title"]);
+        }
+        //adding onepiece
+        elseif($row["typeofclothing"] == "Onepiece")
+        {
+            $one_piece = new Onepiece($row["nameofclothing"], $row["typeofclothing"], $row["color"], $row["pattern"], $row["occasion"], $row["file_title"]);
+        }
+    }
+}
+
 
 
