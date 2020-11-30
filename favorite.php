851d5489a5e093cadbc378634608be86b29c0ca3
vale5181 <vale5181@colorado.edu>
2020-11-30 13:45:11 -0700

    favorite.php working, need to make look better

--- favorite.php
+++ favorite.php
@@ -1,7 +1,79 @@
 <?php
-include("includes/session.php")
+include("includes/session.php");
+
+include_once 'includes/database_setup.php';
+$sql = "SELECT outfitname, piece1, piece2, piece3, temperature, occasion FROM outfits;";
+$stmt = mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $sql))
+{
+    echo 'SQL connection fail';
+}
+else{
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        echo 
+        '<div class="center">
+            <div class = "fav_outfits">
+                <div class="text=center">
+                    <h6>'.$row["outfitname"].'</h6>
+                    <h6>'.$row["temperature"].'</h6>
+                    <h6>'.$row["occasion"].'</h6>
+                </div>
+            </div>
+            <div class = "fav_outfits">
+                <img src="includes/images/' .$row["piece1"].'">
+            </div>';
+
+            if($row['piece2'] != "NULL")
+            {
+                echo '<div class = "fav_outfits">
+                    <img src="includes/images/' .$row["piece2"].'">
+                </div>';
+            }
+            if($row['piece3'] != "NULL")
+            {
+                echo '<div class = "fav_outfits">
+                    <img src="includes/images/' .$row["piece3"].'">
+                </div>';
+            }
+
+        echo '</div>';
+    }
+
+}
 ?>
 
+
+
+
+
+<!-- <div class="center">
+<div class = "fav_outfits">
+    <img src= "includes/images/blue_jacket.PNG" alt="jacket">
+    <div class="text-center">
+        <h6>Hello!</h6>
+        <h6>Hello!</h6>
+    </div>
+</div>
+
+<div class = "fav_outfits">
+    <img src= "includes/images/blue_jacket.PNG" alt="jacket">
+</div>
+
+<div class = "fav_outfits">
+    <img src= "includes/images/blue_jacket.PNG" alt="jacket">
+</div>
+<div class = "fav_outfits">
+    <img src= "includes/images/blue_jacket.PNG" alt="jacket">
+</div>
+</div>
+
+<div>
 <div class = "fav_outfits">
     <img src= "includes/images/blue_jacket.PNG" alt="jacket">
 </div>
@@ -13,3 +85,4 @@ include("includes/session.php")
 <div class = "fav_outfits">
     <img src= "includes/images/blue_jacket.PNG" alt="jacket">
 </div>
+</div> -->
