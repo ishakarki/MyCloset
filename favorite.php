5b3d4d313c0af36b792657c98014a73b06d6bae8
vale5181 <vale5181@colorado.edu>
2020-12-08 11:36:23 -0700

    comments and clean up

--- favorite.php
+++ favorite.php
@@ -1,23 +1,25 @@
 <?php
 #Display the favorite outfit the user saved
 include("includes/session.php");
-
+#include to connect to database
 include_once 'includes/database_setup.php';
+#set up sql statement
 $sql = "SELECT outfitname, piece1, piece2, piece3, temperature, occasion FROM outfits;";
 $stmt = mysqli_stmt_init($conn);
-
+#check if the sql statement failed
 if(!mysqli_stmt_prepare($stmt, $sql))
 {
     echo 'SQL connection fail';
 }
 else{
     mysqli_stmt_execute($stmt);
-
+    #get the result of the statement
     $result = mysqli_stmt_get_result($stmt);
     $occasion = "";
-   
+    #display every row
     while($row = mysqli_fetch_assoc($result))
     {
+        #check the occasion to display text depending on the occasion
         if($row["occasion"] ==1)
         {
             $occasion = "A walk in the park type of casual";
@@ -31,6 +33,7 @@ else{
         elseif ($row["occasion"] ==4){
             $occasion = "Fancy";
         }
+        #html displaying the information of the outfit and clothing pics
         echo 
         '<div class="center">
             <div class = "fav_outfits">
